cba3348a0859483bda71f9aef708ff946e6b6cfe
cster143 <cster143@gmail.com>
2018-06-26 19:30:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,6 +10,14 @@ class App < Sinatra::Base
     (num1*num1).to_s
   end
   get '/say/:number/:phrase' do
-    nmb=params[:number].to_i
-    phr=params[:phrase].to_s
+    @nmb=params[:number].to_i
+    @phr=params[:phrase].to_s
+  end
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @w1=params[:word1]
+    @w2=params[:word2]
+    @w3=params[:word3]
+    @w4=params[:word4]
+    @w5=params[:word5]
+    "#{w1}#{w2}#{w3}#{w4}#{w5}"
   end
