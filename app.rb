604e2f49590dd093dabe793dbfc5c0dfaa72b868
cster143 <cster143@gmail.com>
2018-06-26 19:57:01 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,6 +9,17 @@ class App < Sinatra::Base
     num1=params[:number].to_i
     (num1*num1).to_s
   end
+  get '/say/:number/:phrase' do
+    final_string = ""
+    (params[:number].to_i).times do
+      final_string += "#{params[:phrase]}\n"
+    end
+    final_string
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
 
   end
   get '/:operation/:number1/:number2' do
